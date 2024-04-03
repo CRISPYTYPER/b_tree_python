9c19e9e36d70e6ee1c3cf088e4df3262f11375ec
CRISPYTYPER <jack3409@naver.com>
2024-04-04 00:42:08 +0900

    search and insert done

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 class BTreeNode:
-    def __init__(self, keys, children, is_leaf):
-        self.keys = keys
+    def __init__(self, key_value_list, children, is_leaf):
+        """
+        Initialize a BTreeNode.
+
+        :param key_value_list: A list of tuples(key, value).
+        :param children: A list of children(BTreeNode).
+        :param is_leaf: A boolean value representing whether it is a leaf node.
+        """
+        self.key_value_list = key_value_list
         self.children = children
         self.is_leaf = is_leaf
 
@@ -27,9 +34,9 @@ class BTree:
         :return: A tuple (node, index) where 'node' is the node containing the key 'k', and its index is 'i'. Returns None if 'k' is not found.
         """
         i = 0
-        while i < len(x.keys) and k > x.keys[i]:
+        while i < len(x.key_value_list) and k > x.key_value_list[i][0]:
             i = i + 1
-        if i < len(x.keys) and k == x.keys[i]:
+        if i < len(x.key_value_list) and k == x.key_value_list[i][0]:
             return x, i
         elif x.is_leaf:
             return None
@@ -51,21 +58,52 @@ class BTree:
         """
         y = x.children[i]
         z = BTreeNode([], [], y.is_leaf)
-        z.keys.extend(y.keys[self.t: 2 * self.t - 1])
+        z.key_value_list.extend(y.key_value_list[self.t: 2 * self.t - 1])
         if not y.is_leaf:
             z.children.extend(y.children[self.t: 2 * self.t])
-        x.children.insert(i+1, z)
-        x.keys.insert(i, y.keys[self.t])
+        x.children.insert(i + 1, z)
+        x.key_value_list.insert(i, y.key_value_list[self.t])
 
-    def b_tree_insert(self, k):
+    def b_tree_insert(self, k, v):
+        """
+        Insert a key k and v pair(tuple) into the B-tree in a single pass down the tree.
+        The b_tree_insert procedure uses b_tree_split_child to guarantee that the recursion never descends to a full node.
+        :param k: A key to insert.
+        :param v: A value to insert.
+        :return: None. This function performs its operation without returning a value.
+        """
         r = self.root
-        if len(self.root.keys) == 2 * self.t - 1:
+        if len(self.root.key_value_list) == 2 * self.t - 1:
             s = BTreeNode([], [], False)
             self.root = s
             s.children[0] = r
             self.b_tree_split_child(s, 0)
-            self.b_tree_insert_nonfull(s, k)
+            self.b_tree_insert_nonfull(s, k, v)
         else:
-            self.b_tree_insert_nonfull(r, k)
+            self.b_tree_insert_nonfull(r, k, v)
 
-    # TODO: make b_tree_insert_nonfull()
+    def b_tree_insert_nonfull(self, x, k, v):
+        """
+        Insert key k and v pair(tuple) into the tree rooted at the nonfull root node.
+        b_tree_insert_nonfull recurses as necessary down the tree, at all times guaranteeing
+        that the node to which it recurses is not full by calling b_tree_split_child as necessary.
+
+        :param x: A node to insert.
+        :param k: A key to insert into node x.
+        :param v: A value to insert into node x.
+        :return: None. This function performs its operation without returning a value.
+        """
+        i = len(x.key_value_list) - 1
+        if x.is_leaf:
+            while i >= 0 and k < x.key_value_list[i][0]:
+                x.key_value_list[i + 1] = x.key_value_list[i]
+                i = i - 1
+            x.key_value_list[i + 1] = k, v
+        else:
+            while i >= 0 and k < x.key_value_list[i][0]:
+                i = i - 1
+            if len(x.children[i].key_value_list) == 2 * self.t - 1:
+                self.b_tree_split_child(x, i)
+                if k > x.key_value_list[i][0]:
+                    i = i + 1
+            self.b_tree_insert_nonfull(x.children[i], k, v)
